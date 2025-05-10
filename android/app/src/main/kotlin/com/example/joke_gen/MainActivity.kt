5dce36efca63f175fa82edeec145840dd60537cb
Yashas H Majmudar <yashashm.dev@gmail.com>
2025-05-10 17:56:33 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/joke_gen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.joke_gen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/joke_generator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JokeGeneratorScreen extends StatefulWidget {
+  const JokeGeneratorScreen({super.key});
+
+  @override
+  State<JokeGeneratorScreen> createState() => _JokeGeneratorScreenState();
+}
+
+class _JokeGeneratorScreenState extends State<JokeGeneratorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
